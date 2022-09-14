efa5653e766618400c7940f876be5a8f867b3894
ken <himzei@gmail.com>
2022-09-14 16:33:21 +0900

    a

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    // Slick Slider
+    $(".slideshow").slick({
+      infinite: true,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      fade: true,
+      speed: 1000,
+      pauseOnHover: false,
+    });
+})
