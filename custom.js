f6a5c5b38ec661be825108970a6b13ed125b00a2
조현일 <himzei@gmail.com>
2022-09-15 08:41:04 +0900

    new versiton velocity

---
+++ custom.js
@@ -0,0 +1,22 @@
+$(function () {
+  // Tirgger
+  $(".trigger").click(function () {
+    $(this).toggleClass("active");
+    $(".gnb").toggleClass("active");
+  });
+  $("section, .menu a").click(function () {
+    $(".gnb").removeClass("active");
+    $(".trigger").removeClass("active");
+  });
+
+  // Header Nav
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $("header").addClass("active");
+      $(".gototop").addClass("active");
+    } else {
+      $("header").removeClass("active");
+      $(".gototop").removeClass("active");
+    }
+  });
+});
